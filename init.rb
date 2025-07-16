cf05f148afb2a838d5ef8009d1f1305649c703ce
Cevin Eichnau <cevin@eichnau.com>
2025-07-16 15:19:17 +0200

    Add init.rb to load resources

---
+++ init.rb
@@ -0,0 +1,6 @@
+require_relative 'res/gameobject'
+require_relative 'res/gamefield'
+require_relative 'res/player'
+require_relative 'res/enemy'
+require_relative 'res/item'
+require_relative 'res/node'
